d120ae9773a7f1017a8d8d7a00220aefd7b2f2cf
NotUsedBrain <notusedbrain@hotmail.com>
2019-09-13 15:25:03 +0100

    fixed nickname and still learning about Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:index, :show]
+  protect_from_forgery with: :exception
+  before_action :configuration, if: :devise_controller?
+
+protected
+
+  def configuration
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
 end
