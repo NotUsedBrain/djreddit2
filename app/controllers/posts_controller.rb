2188f6b3d8966f91348076a9a51bc20dba555585
NotUsedBrain <notusedbrain@hotmail.com>
2019-09-15 11:40:41 +0100

    posts in communities - fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,21 @@
 class PostsController < ApplicationController
-
-  def new
-    @post = Post.new
-  end
-
+ 
   def create
     @community = Community.find(params[:community_id])
-    @post = @community.posts.create(params[:post].permit(post_params))
+    @post = @community.posts.create(params[:post].permit(:title, :content))
 
     redirect_to community_path(@community)
+  end
+
+  def destroy
+    @community = Community.find(params[:community_id])
+    @post = @community.posts.find(params[:id])
+    @post.destroy
+    redirect_to post_path(@post)
   end
 
   def show
     @post = Post.find(params[:id])
   end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :content)
   end
-
-end
+ 
